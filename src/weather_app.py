407011b2fdd3a1b9087c1e5400c39e0352aed950
Matheus Fernando Vieira Pinto <matheus.fvp@outlook.com>
2024-08-16 16:57:28 -0300

    feat - get the user's latitude and longitude

--- src/weather_app.py
+++ src/weather_app.py
@@ -1,8 +1,12 @@
+import json
+# import pprint
 import requests
 
-
 request = requests.get('http://www.geoplugin.net/json.gp')
+
 if request.status_code != 200:
     print('Não foi possivel obter a localização')
 else:
-    print(request.text)
+    location = json.loads(request.text)
+    latitude = location['geoplugin_latitude']
+    longitude = location['geoplugin_longitude']
